ee199bd90335f9a1e6bcde5464902de13ae36df4
Marco van den Oever <marco.vandenoever@infosupport.com>
2019-11-03 18:09:39 +0100

    Simple demo

--- ClusterSender/Actors/AMediatorSender.cs
+++ ClusterSender/Actors/AMediatorSender.cs
@@ -1,29 +1,30 @@
 using Akka.Actor;
 using Akka.Cluster.Tools.PublishSubscribe;
+using Akka.Event;
 
 namespace ClusterSender.Actors
 {
-    public class AMediatorSender: ReceiveActor
-    {
-        public AMediatorSender()
-        {
-            Receive<string>(s =>
-            {
-                var mediator = DistributedPubSub.Get(Context.System).Mediator;
-                mediator.Tell(new Put(Self));
-                try
-                {
-                    DistributedPubSub.Get(Context.System).Mediator.Tell(new Send("/user/mediatedListener/", s));
-                    Context.ActorSelection("akka.tcp://ClusterSys@localhost:4051/user/mediatedListener")
-                        .Tell($"direct message to actor ref: {s}");
-                    mediator.Tell(new Send("/user/mediatedSender", s));
-                    Sender.Tell(true);
-                }
-                catch
-                {
-                    Sender.Tell(false);
-                }
-            });
-        }
-    }
+	public class AMediatorSender : ReceiveActor
+	{
+		public AMediatorSender()
+		{
+			Receive<string>(s =>
+			{
+				var mediator = DistributedPubSub.Get(Context.System).Mediator;
+				mediator.Tell(new Put(Self));
+				try
+				{
+					Context.GetLogger().Info("SEND");
+
+					mediator.Tell(new Send("/user/ping", s));
+
+					Sender.Tell(true);
+				}
+				catch
+				{
+					Sender.Tell(false);
+				}
+			});
+		}
+	}
 }
--- ClusterSender/Program.cs
+++ ClusterSender/Program.cs
@@ -1,39 +1,34 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using Akka.Actor;
+﻿using Akka.Actor;
 using Akka.Cluster.Tools.PublishSubscribe;
 using Akka.Configuration;
-using Akka.Routing;
-using ClusterSender.Actors;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ClusterSender
 {
-    internal static class Program
-    {
-        private static async Task Main()
-        {
-            Console.WriteLine("Starting sender system...");
-            var system = ActorSystem.Create("ClusterSys",
-                ConfigurationFactory.ParseString(File.ReadAllText("Akka.hocon")));
-            var sender = system.ActorOf(Props.Create<AMediatorSender>().WithRouter(FromConfig.Instance), 
-                "mediatedSender");
-            Console.ReadKey();
-            var message = "initial message";
-            while (!message.Equals("Stop"))
-            {
-                Console.WriteLine("New message:");
-                message = Console.ReadLine();
-//                Console.WriteLine(await sender.Ask<bool>(message) switch
-//                    {
-//                        true => "Sent ok",
-//                        false => "Send failed"
-//                    });
-                DistributedPubSub.Get(system).Mediator.Tell(new Send("/user/ping", message, true));
-            }
-            Console.WriteLine("Terminating system.");
-            await system.Terminate();
-            Console.WriteLine("Bye...");
-        }
-    }
+	internal static class Program
+	{
+		private static async Task Main()
+		{
+			Console.WriteLine("Starting sender system...");
+			var system = ActorSystem.Create("ClusterSys", ConfigurationFactory.ParseString(File.ReadAllText("Akka.hocon")));
+			Console.ReadKey();
+
+			var message = "initial message";
+			var mediator = DistributedPubSub.Get(system).Mediator;
+
+			while (!message.Equals("Stop"))
+			{
+				Console.WriteLine("New message:");
+				message = Console.ReadLine();
+
+				mediator.Tell(new Send("/user/ping", message, false));
+			}
+
+			Console.WriteLine("Terminating system.");
+			await system.Terminate();
+			Console.WriteLine("Bye...");
+		}
+	}
 }
